b8088293b23beb3dcc58fa9fa7634b2634de252f
alejandro <alejandro.solano@jalasoft.com>
2023-06-21 17:05:11 -0400

    update features file path in runnertest file

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
 //        features = {"C:\\Users\\aleja\\IdeaProjects\\SerenityRestAssured002\\src\\test\\resources\\features\\resgister_user.feature"},
-        features = {".\\src\\test\\resources\\features\\resgister_user.feature"},
+        features = {"src\\test\\resources\\features\\resgister_user.feature"},
         glue = {"stepDefinition"},
         plugin = {"pretty"}
 )
